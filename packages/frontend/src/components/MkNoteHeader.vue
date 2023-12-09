ce5f4da6fc6a1f7fb9a2dd2af90baad09b4daa88
Suzulu <zukkiyb.iu2@gmail.com>
2023-12-10 00:42:52 +0900

    noteで広告である旨を表示

--- packages/frontend/src/components/MkNoteHeader.vue
+++ packages/frontend/src/components/MkNoteHeader.vue
@@ -31,7 +31,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 		<span v-if="note.localOnly" style="margin-left: 0.5em;" :title="i18n.ts._visibility['disableFederation']"><i class="ti ti-rocket-off"></i></span>
 		<span v-if="note.channel" style="margin-left: 0.5em;" :title="note.channel.name"><i class="ti ti-device-tv"></i></span>
 		<span v-if="note.channel && !note.channel.searchable" style="margin-left: 0.5em;" :title="i18n.ts.noteNotSearchable"><i class="ti ti-search-off"></i></span>
-		<span v-if="isPR == true" style="margin-left: 0.5em;"><i class="ti ti-badge-ad"></i>プロモーション</span>
+		<span v-if="note.isPR == true" style="margin-left: 0.5em;"><i class="ti ti-badge-ad"></i>プロモーション</span>
 	</div>
 </header>
 </template>
